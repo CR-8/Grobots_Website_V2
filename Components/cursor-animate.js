bfcddca05cde297ebdbd7559f3e1900324dbc78b
Pratyush <pratyushtiwari446@gmail.com>
2024-11-20 19:48:07 +0530

    Commit 2 Updated , added Components folder

---
+++ Components/cursor-animate.js
@@ -0,0 +1,16 @@
+import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js';
+
+    neonCursor({
+      el: document.getElementById('app'),
+      shaderPoints: 10,
+      curvePoints: 80,
+      curveLerp: 0.9,
+      radius1: 5,
+      radius2: 20,
+      velocityTreshold: 50,
+      sleepRadiusX: 0,
+      sleepRadiusY: 0,
+      sleepTimeCoefX: 0.0025,
+      sleepTimeCoefY: 0.001,
+      colors: ['#ff0000', '#00ff00', '#0000ff', '#ff00ff', '#ffff00']
+    });
